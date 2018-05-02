2dab8f2f720b888f67438fa47c01652bb19f13f0
bojancvoro <bojan.cvoro@gmail.com>
2018-05-02 02:56:13 +0200

    add methods to get data on location and weather

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import DisplayWeather from './components/displayWeather';
 
+// get data on weather and store it, get location data and store it, render DisplayWeather component
+// and pass it data
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: "",
+      lat: "",
+      lon: "",
+      description: "",
+      temp: "",
+      changing: false
+    }
+    console.log("inicijalni state:", this.state);
+  }
+
+  componentDidMount() {
+    this.getLocation();
+  }
+  
+  getLocation() {
+    // gets user city and coordinates and set them to state - DONE
+    // consider adding more elaborate error handler
+    fetch("http://ip-api.com/json")
+      .then(response => response.json())
+      .then(data => {
+        const city = data.city;
+        const lat = data.lat;
+        const lon = data.lon;
+        this.setState({ city, lat, lon });
+      })
+      .then(() => console.log("state nakon location fetch-a:", this.state))
+      .then(() => this.getWeatherData());
+  }
+  
+  getWeatherData() {
+    // gets temperature and weather description for the given coordinates
+    const keyID = "4e51570e41fb01c6";
+    fetch(`http://api.wunderground.com/api/4e51570e41fb01c6/conditions/q/${this.state.lat},${this.state.lon}.json`)
+      .then(response => response.json())
+      .then(data => {
+        const temp = data.current_observation.temp_c;
+        const description = data.current_observation.weather;
+        this.setState({ description, temp, changing: true });
+      } )
+      .then(() => console.log("state nakon weather fetch-a:", this.state));
+  }
+
   render() {
     return (
       <div className="App">
+        <p>sampletext</p>
         
       </div>
     );
---
+++ src/components/displayWeather.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DisplayWeather = (props) => {
+
+}
+
+export default DisplayWeather;
